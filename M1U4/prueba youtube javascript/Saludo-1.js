cc4dc613fe18533e775a66a51d06f6ec19bf53a6
Estefanía Zakovicz <estefaniazakovicz@hotmail.com>
2023-03-01 01:28:31 -0300

    U1M4

---
+++ M1U4/prueba youtube javascript/Saludo-1.js
@@ -0,0 +1,11 @@
+var totalComida = 2800 + 1300;
+var totalBedida = 900 + 650;
+
+var total = totalComida + totalBedida;
+console.log("El total de comida es: " + totalComida);
+console.log("El total de bebida es: " + totalBedida)
+console.log("El total de la cuenta es: " + total);
+
+
+
+
